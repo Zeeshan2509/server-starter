6a139fcc78b58b644d4805ed2000fdadce0c83bd
Zeeshan2509 <muhammadzeeshanshafqat2008@gmail.com>
2026-01-29 20:40:51 +0500

    Create status.py

---
+++ status.py
@@ -0,0 +1,30 @@
+import socket
+import struct
+import sys
+import os
+
+SERVER_IP = os.environ.get('SERVER_IP')
+PORT = int(os.environ.get('SERVER_PORT'))
+
+def check_bedrock():
+    ping_packet = bytearray.fromhex("01000000000000000000ffff00fefefefefdfdfdfd12345678")
+    ping_packet.extend(struct.pack('>q', 0))
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(3.0)
+    
+    try:
+        sock.sendto(ping_packet, (SERVER_IP, PORT))
+        data, addr = sock.recvfrom(1024)
+        if data:
+            return "ONLINE"
+    except:
+        pass
+    finally:
+        sock.close()
+    return "OFFLINE"
+
+if __name__ == "__main__":
+    result = check_bedrock()
+    print(result)
+    sys.exit(1 if result == "ONLINE" else 0)
