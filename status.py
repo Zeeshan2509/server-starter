cc4ad9906accc0c2a12f54e35806ad19eb0bad3c
Zeeshan2509 <muhammadzeeshanshafqat2008@gmail.com>
2026-02-10 17:03:38 +0500

    Update status.py

--- status.py
+++ status.py
@@ -3,12 +3,10 @@ import struct
 import sys
 import os
 
-# --- CONFIGURATION (PULLED FROM SECRETS) ---
 SERVER_IP = os.environ.get('SERVER_IP')
 PORT = int(os.environ.get('SERVER_PORT'))
 
 def check_bedrock():
-    # Bedrock Unconnected Ping Packet
     ping_packet = bytearray.fromhex("01000000000000000000ffff00fefefefefdfdfdfd12345678")
     ping_packet.extend(struct.pack('>q', 0))
     
@@ -28,7 +26,5 @@ def check_bedrock():
 
 if __name__ == "__main__":
     result = check_bedrock()
-    # Print for YAML to capture
     print(result)
-    # Always exit 0 so GitHub doesn't show a Red Error
     sys.exit(0)
